5eace5f084ad6cee114e75e6b2418eda95569f72
jasonwidrig <jasonwidrig@gmail.com>
2020-09-10 09:14:11 -0700

    Create sfx.js
    Copied straight from current lambda

---
+++ lambda/sfx.js
@@ -0,0 +1,19 @@
+
+const LAUNCH_TONES = ["soundbank://soundlibrary/musical/amzn_sfx_electronic_beep_03"];
+const RIGHT_SOUNDS = ["soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_03",
+                        "soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_02",
+                        "soundbank://soundlibrary/musical/amzn_sfx_bell_short_chime_01",
+                        "soundbank://soundlibrary/musical/amzn_sfx_bell_med_chime_02"];
+const WRONG_SOUNDS = ["soundbank://soundlibrary/musical/amzn_sfx_buzzer_small_01",
+                        "soundbank://soundlibrary/alarms/buzzers/buzzers_06",
+                        "soundbank://soundlibrary/alarms/buzzers/buzzers_09",
+                        "soundbank://soundlibrary/musical/amzn_sfx_buzzer_loud_alarm_01",
+                        "soundbank://soundlibrary/buzzers_pistols/buzzers_pistols_01"];
+
+const sfx = {
+    launchTones:LAUNCH_TONES,
+    rightSounds:RIGHT_SOUNDS,
+    wrongSounds:WRONG_SOUNDS,
+};
+
+exports.sfx = sfx;
